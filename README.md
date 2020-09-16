9f964e3d866a97b6e3e2f7132f5774faded5c2a2
aman <bora.amanalambora@gmail.com>
2020-09-16 21:30:00 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Node.js NotchUp
+
+In order to run the application:
+
+1. Install dependencies by executing: `npm install` or `yarn`.
+2. Start the server by executing `node index.js`
+3. Navigate to http://localhost:8080 on your browser.
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import the express lirbary
+const express = require('express')
+const https = require('https')
+
+
+const app = express()
+app.use(express.static(__dirname + '/public'))
+
+
+// Start the server on port 8080
+app.listen(8080, ()=>{
+  console.log("Listening at port 8080");
+})
