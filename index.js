29ada74cabc5a8ebec8fafbfd1f77afe3fa604bb
aman <bora.amanalambora@gmail.com>
2020-09-16 22:02:27 +0530

    bugs

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const https = require('https')
 const app = express()
 app.use( express.static(__dirname + '/public'))
 
-app.get('/', (req, res)=>{
+app.get('*', (req, res)=>{
   res.render('index')
 })
 // Start the server on port 8080
-app.listen(8080, ()=>{
+app.listen(process.env.PORT || 8080, ()=>{
   console.log("Listening at port 8080");
 })
